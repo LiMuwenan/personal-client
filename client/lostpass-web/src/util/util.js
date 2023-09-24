a1e2ef315fb921a40e4b6cf5ff02f0b003dbf736
ligen <bear.gen.li@outlook.com>
2023-09-24 15:18:34 +0800

    feat: get post请求对应后端LocalDateTime

--- client/lostpass-web/src/util/util.js
+++ client/lostpass-web/src/util/util.js
@@ -8,14 +8,14 @@ export function toast(message, type = "success") {
     })
 }
 
-export function coverterTime(rawTime) {
+export function coverterTime(rawTime, method) {
     let rawDate = new Date(rawTime);
     let date = rawDate.getFullYear()
         + "-"
         + ('0' + (rawDate.getMonth() + 1)).slice(-2)
         + "-"
         + ('0' + rawDate.getDate()).slice(-2)
-        + " " 
+        + 'T'
         + ('0' + rawDate.getHours()).slice(-2)
         + ":" 
         + ('0' + rawDate.getMinutes()).slice(-2)
